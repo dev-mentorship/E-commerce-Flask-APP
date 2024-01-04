30bd04435e1b60ca813e70460f3d9a41272934d8
Idris Fagbemi <idrisniyi94@gmail.com>
2024-01-03 18:52:32 -0600

    Added products to the shop list

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, url_for, request, redirect
+import requests
 
 app = Flask(__name__)
 
+
+def get_products():
+    data = requests.get('https://dummyjson.com/products')
+    return data.json()['products']
+
+# print(get_products())
 @app.route('/index')
 @app.route('/', methods=['GET'])
 def index():
@@ -27,6 +34,36 @@ def login():
     # print(f"Username: {usrEmail}, password: {usrPassword}")
     return render_template('login.html')
 
+@app.route('/shop-grid')
+def shop_grid_view():
+    return render_template('shop-grid-view.html')
+
+@app.route('/shop-list')
+def shop_list_view():
+    products = get_products()
+
+
+    return render_template('shop-list-view.html', products=products)
+
+# @app.route('/wishlist')
+# def wishlist_style1():
+#     return render_template('wishlist-style1.html')
+
+@app.route('/products')
+def products():
+    return render_template('product-layout1.html')
+
+@app.route('/checkout')
+def checkout_style1():
+    return render_template('checkout-style1.html')
+
+@app.route('/cart')
+def cart_style1():
+    return render_template('cart-style1.html')
+
+@app.route('/order-success')
+def order_success():
+    return render_template('order-success.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host="0.0.0.0")
